86ddaa30e6fbb6d421ea319118e4338eb2c284e8
volkova-lisa <liza.volk999@gmail.com>
2021-11-10 15:42:26 +0200

    fixed errors + added view model

--- app/src/main/java/com/example/mvvmvoicerecorder/MainActivity.kt
+++ app/src/main/java/com/example/mvvmvoicerecorder/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.mvvmvoicerecorder
 
+import android.app.ActivityManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.mvvmvoicerecorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!!
+    lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_container)
+    }
+
+    fun isServiceRunning(): Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as
+                ActivityManager
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            if ("info.fandroid.voicerecorder.record.RecordService" ==
+                service.service.className
+            ) {
+                return true
+            }
+        }
+        return false
     }
 }
